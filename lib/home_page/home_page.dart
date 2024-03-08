82a23ebcd1a0c34b5308ee783edd415313e0e1c5
Tasneem AbdElaziz <taelaziz1024@gmail.com>
2024-03-08 20:38:26 +0200

    first commit in home features.

--- lib/home_page/home_page.dart
+++ lib/home_page/home_page.dart
@@ -8,7 +8,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("Home"),
+      child: Text("hello Home"),
     );
   }
 }
